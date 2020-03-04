1f8adfae12e681f49ee49ab053438722af68bf16
Valery <valery.e.akimov@gmail.com>
2020-03-04 15:31:11 +0300

    Few changes (nothing)

--- app/src/main/java/com/example/concentration/SettingsActivity.java
+++ app/src/main/java/com/example/concentration/SettingsActivity.java
@@ -7,10 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.concentration.DataSave.PreferencesUtil;
 
 public class SettingsActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
